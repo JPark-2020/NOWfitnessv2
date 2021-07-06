61aa98b6b59f70f287214491bd6f514941c6f26e
jonathancode <parkj97@gmail.com>
2021-07-05 20:32:56 -0700

    made models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from django.db.models import Model
+from django.contrib.auth.models import User
+from django.db.models.fields.related import ManyToManyField 
 
-# Create your models here.
+
+#1 Profile has 1 user 
+class Profile(Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    join_date = models.DateTimeField(auto_now_add=True)
+    image = models.FileField(upload_to="images", null=True)
+
+    def __str__(self):
+        return self.user.username 
+
+
+class Exercise(Model):
+    name = models.CharField(max_length=50) 
+    image = models.CharField(max_length=5000) 
+    description = models.TextField() 
+
+    def __str__(self):
+        return self.name 
+
+
+#Many Workouts have many exercises. Many Exercises have many workouts. 
+class Workout(Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField()
+    exercises = ManyToManyField(Exercise)
+
+    def __str__(self):
+        return self.name 
+
+#One Tracker has One User 
+#One Tracker has one workout, One workout has many trackers 
+
+class Tracker(Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    tracked_workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
+    date_custom = models.DateTimeField(null=True, blank=True)
+    date_creation = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'{self.user.username}: {self.date_custom} - {self.tracked_workout}'
+
+    
+#One entry has one author. One author has many entries. 
+#One entry has one exercise. One exercise has many entries 
+#One entry has one tracker. One tracker has many entries 
+
+class Entry(Model):
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    entry_exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    tracker = models.ForeignKey(Tracker, on_delete=models.CASCADE)
+    date_custom = models.DateTimeField(null=True, blank=True)
+    date_creation = models.DateTimeField(auto_now_add=True)
+    set_one = models.CharField(max_length=2, default=0, null=True, blank=True)
+    set_two = models.CharField(max_length=2, default=0, null=True, blank=True)
+    set_three = models.CharField(max_length=2, default=0, null=True, blank=True)
+    set_four = models.CharField(max_length=2, default=0, null=True, blank=True)
+    set_five = models.CharField(max_length=2, default=0, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.author}: {self.date_custom} - {self.tracker}'
+
+
+
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from django.http import HttpResponse
+from django.views import View  
+from django.views.generic.base import TemplateView 
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView 
+from django.urls import reverse 
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator 
 
-# Create your views here.
+
+class Home(TemplateView):
+    template_name = "home.html" 
+
